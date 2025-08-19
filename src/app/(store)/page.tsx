e997def6181ba10966b029e47b9d037885b792aa
AmaanIqbal0011 <166907241+AmaanIqbal0011@users.noreply.github.com>
2025-08-19 15:59:08 +0500

    update styling hero Section

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
 
   return (
-    <div className="container mx-auto px-4 py-10">
+    <div className="container mx-auto px-4 py-2">
  
       <HeroSection />
 
